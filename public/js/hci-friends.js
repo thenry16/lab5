49756351609c255f78f4b3dfd89c3e3bf62c3823
Tiphaine Henry <tiphaine.henry02@orange.fr>
2018-02-07 23:21:08 -0800

    Commit for heroku deployment

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+    $(".friends a").click(clickListener);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,9 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+function clickListener(e){
+	e.preventDefault();
+    $(this).text(anagrammedName($(this).text()));
 }
--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,15 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
- }
+    var name = req.query.name;
+    var description = req.query.description;
+    console.log(newFriend);
+    var newFriend = {
+        "name": "New friend",
+        "description": "Friend description",
+        "imageURL":"http://lorempixel.com/400/400/people"
+    };
+    data.friends.push(newFriend);
+    res.render('index',newFriend);
+
+  };
